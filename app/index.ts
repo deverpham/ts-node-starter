8a7ccdcb312ea1ce9a5f7f9eff37b41ddba93d3d
THINH <deverpham@gmail.com>
2019-08-28 09:53:25 +0700

    update missing file

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,7 @@
-console.log('gg');
+import * as Express from 'express';
+const App: Express.Application = Express();
+App.get('/', (req:Express.Request, res:Express.Response) => {
+    res.send('itworks');
+})
+App.listen(2000, () => {
+});
